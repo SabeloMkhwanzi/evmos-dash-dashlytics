6ea38c91731b254c6d7d5fbb1c03611c1e907277
sabelo <67804592+SabeloMkhwanzi@users.noreply.github.com>
2022-12-01 16:45:46 +0200

    updated logo + color

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import { Image } from "@mantine/core"
-import Link from 'next/link'
+import React from "react";
+import { Image } from "@mantine/core";
+import Link from "next/link";
 
-export default  function Logo() {
+export default function Logo() {
   return (
-      <>
-            <Link href="/" >            
-              <Image
-                pb={2}
-              src="/eddalytics-logo2.png"
-              alt="gigiblock-logo"
-              width="12vw"
-              height="calc(7vw*0.3)"
-            />
-            </Link>
+    <>
+      <Link href="/">
+        <Image
+          pb={2}
+          src="/eddalytics-logo11.png"
+          alt="gigiblock-logo"
+          width="12vw"
+          height="calc(7vw*0.3)"
+        />
+      </Link>
     </>
-  )
+  );
 }
-
